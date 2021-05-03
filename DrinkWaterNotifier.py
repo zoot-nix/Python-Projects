460ba27785afa427af9a71a2e6165233a33adb61
zoot-nix <66899497+zoot-nix@users.noreply.github.com>
2021-05-03 23:04:54 +0530

    Updated with comments

--- DrinkWaterNotifier.py
+++ DrinkWaterNotifier.py
@@ -1,16 +1,22 @@
 import time
 from datetime import datetime
-from plyer import notification
+from plyer import notification #for notifications on pc
 
 dt = datetime.now()
 current_time = dt.strftime("%H:%M:%S")
 
 notification.notify(
+#title of notification
 title = "Drink Water Reminder",
+#message of the notification
 message = "This is a Reminder for you to Drink 2 Glasses of Water\nTime: "+current_time+"\nNote: Next Reminder will be after 1 hr.",
 
+#icon for notification
+#download water.ico from repo
 app_icon = "water.ico",
 
+#notification stays for 50 secs
 timeout = 50
 )
-time.sleep(60*60)#1 hr
+#repeats in 1 hr
+time.sleep(60*60)
